b222127bc70ee576adfbd454cf7656c4100bea6a
Oleksii Khrustalov <oleksiwe22@gmail.com>
2024-05-11 17:44:39 +0300

    added and styled sections for Home page

--- client/src/components/Home/Feature.jsx
+++ client/src/components/Home/Feature.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Feature = (props) => {
+function Feature({ title, content, icon: Icon }) {
     return (
         <div className="feature">
-            <h4>{ props.title }</h4>
-            <p>
-                { props.content }
+            <Icon />
+            <h4 className="feature__title">{ title }</h4>
+            <p className="feature__content">
+                { content }
             </p>
         </div>
     )

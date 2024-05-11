4101519b7eb5ec984a7c293ed7f1f81798359f16
Shemberko <sasha.shemberko15@gmail.com>
2024-05-11 15:36:16 +0300

    added user context

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,37 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./components/Layouts/Header";
 import Footer from "./components/Layouts/Footer";
+import { useContext, useEffect, useMemo, useCallback } from 'react';
+
+export const UserContext =useContext(null);
 
 function App() {
+  const [user, setUser] = useState(null);
+  const login = useCallback((newUser) => {
+    setUser(newUser);
+  }, []);
+
+  const contextValue = useMemo(() => ({
+    user,
+    login
+  }), [user, login]);
+
+  useEffect(() => {
+    if(user){
+      localStorage.setItem('appState', JSON.stringify(user));
+    }
+   
+  }, [user]);
+
+  useEffect(() => {
+    const savedState = localStorage.getItem('appState');
+    if (savedState) {
+      login(JSON.parse(savedState));
+    }
+  }, []);
+
   return (
+    <UserContext.Provider value={{contextValue}}>
       <BrowserRouter>
         <Header />
         <Routes>
@@ -14,6 +42,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 

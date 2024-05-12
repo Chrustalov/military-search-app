5699bd237c05abc143054dd6a8affebbb886f9f3
Oleksii Khrustalov <oleksiwe22@gmail.com>
2024-05-12 06:37:47 +0300

    fix registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,6 +15,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def respond_with(current_user, _opts = {})
+    binding.pry
     if resource.persisted?
       render json: {
         status: {code: 200, message: 'Signed up successfully.'},
@@ -36,25 +37,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   # def create
-  #   # build_resource(sign_up_params)
-
-  #   # resource.save
-  #   # yield resource if block_given?
-  #   # if resource.persisted?
-  #   #   if resource.active_for_authentication?
-  #   #     set_flash_message! :notice, :signed_up
-  #   #     sign_up(resource_name, resource)
-  #   #     respond_with resource, location: after_sign_up_path_for(resource)
-  #   #   else
-  #   #     set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-  #   #     expire_data_after_sign_in!
-  #   #     respond_with resource, location: after_inactive_sign_up_path_for(resource)
-  #   #   end
-  #   # else
-  #   #   clean_up_passwords resource
-  #   #   set_minimum_password_length
-  #   #   respond_with resource
-  #   # end
+  #   build_resource(sign_up_params)
+  #
+  #   resource.save
+  #   yield resource if block_given?
+  #   if resource.persisted?
+  #     if resource.active_for_authentication?
+  #       set_flash_message! :notice, :signed_up
+  #       sign_up(resource_name, resource)
+  #       respond_with resource, location: after_sign_up_path_for(resource)
+  #     else
+  #       set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+  #       expire_data_after_sign_in!
+  #       respond_with resource, location: after_inactive_sign_up_path_for(resource)
+  #     end
+  #   else
+  #     clean_up_passwords resource
+  #     set_minimum_password_length
+  #     respond_with resource
+  #   end
   # end
 
   # GET /resource/edit
